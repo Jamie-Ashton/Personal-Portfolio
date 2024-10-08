05bc9d38d91c2bd4341a28da2a83caf3e9bb3bb8
Jamie-Ashton <110884625+Jamie-Ashton@users.noreply.github.com>
2024-10-09 11:58:37 +1300

    update changing contact page

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 still working on the correct file structure when I set it up like you want it bellows my icon up and i have not idea why
 
+change enter you email to a log in button
--- scripts/authUser.js
+++ scripts/authUser.js
@@ -1,13 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const contactLink = document.getElementById("contact");
-    if (contactLink) {
-        contactLink.addEventListener('click', (event) => {
-            event.preventDefault(); // Prevent the default action
-            console.log('Contact Me link clicked');
-            alert('Contact Me link clicked'); // Add a pop-up box
-            // You can add your custom logic here
-        });
-    } else {
-        console.error('Contact link not found');
-    }
-});
+import { app } from "./firebaseSDK.js";
+import { GoogleAuthProvider, getAuth, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+
+
+
+const provider = new GoogleAuthProvider();
